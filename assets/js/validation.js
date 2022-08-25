71afcbf9ea94e505c2b70f0b79b4cf2e3e4e1628
swineethamurthy <swineethamurthy@gmail.com>
2022-08-25 16:36:23 +0530

    feat:validation using HTML5 attributes

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,45 +1,18 @@
-function isValidName() {
-  var regex = /^[A-Za-z ]+$/;
-  var inputName = document.getElementById("name").value;
-  if (!regex.test(inputName) || inputName.length >= 15) {
-    showValidation(
-      "name-validation",
-      "Name can have only spaces and Alphabets and of maximum length 15."
-    );
-  } else {
-    document.getElementById("name-validation").style.display = "none";
-  }
-}
+/*
+=========================================
+function to use with HTML 5 attributes
+=========================================
+*/
+function chkValidName() {
+  const nameField = document.querySelector("input");
 
-function isValidEmail() {
-  var inputEmail = document.getElementById("email").value;
-  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail)) {
-    showValidation("email-validation", "Please enter a valid email ID");
-  } else {
-    document.getElementById("email-validation").style.display = "none";
-  }
-}
+  nameField.addEventListener("input", () => {
+    nameField.setCustomValidity("");
+    nameField.checkValidity();
+    console.log(nameField.checkValidity());
+  });
 
-function chkCommentLength() {
-  var inputComment = document.getElementById("comments").value;
-  if (inputComment.length != 0 && inputComment.length >= 200) {
-    showValidation(
-      "comment-validation",
-      "Comments can have maximum length of 200."
-    );
-  } else {
-    document.getElementById("comment-validation").style.display = "none";
-  }
-}
-
-function showValidation(toValidate, validationMessage) {
-  var elementToValidate = document.getElementById(toValidate);
-  elementToValidate.style.display = "block";
-  elementToValidate.innerText = validationMessage;
-}
-
-function formValidation() {
-  isValidName();
-  isValidEmail();
-  chkCommentLength();
+  nameField.addEventListener("invalid", () => {
+    nameField.setCustomValidity("Name can contain only letters and spaces");
+  });
 }
